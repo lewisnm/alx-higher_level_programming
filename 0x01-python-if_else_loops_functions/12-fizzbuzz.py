d762aba4f89421dab5926b5ed25f6cf295bb9bf8
Lewis Mutiso <lewisnmutiso@gmail.com>
2024-01-02 16:39:04 -0800

    10, 11 and 12

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for no in range(1, 101):
+        if no % 3 == 0 and no % 5 == 0:
+            print("FizzBuzz ", end="")
+        elif no % 5 == 0:
+            print("Buzz ", end="")
+        elif no % 3 == 0:
+            print("Fizz ", end="")
+        else:
+            print(f"{no} ", end="")
