69070fc7ff1c5a744d8933fc3a503ba1935415ff
Lewis Mutiso <lewisnmutiso@gmail.com>
2024-01-04 15:39:40 -0800

    a Python function def magic_calculation(a, b): that does exactly the same as the following Python bytecode

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
+
+
 def magic_calculation(a, b):
+    """Match bytecode provided by ALX."""
+    from magic_calculation_102 import add, sub
+
     if a < b:
-        add = magic_calculation_102.add
-        sub = magic_calculation_102.sub
         c = add(a, b)
         for i in range(4, 6):
             c = add(c, i)
-        return c
-    else:
-        return magic_calculation_102.sub(a, b)
+        return (c)
 
+    else:
+        return(sub(a, b))
