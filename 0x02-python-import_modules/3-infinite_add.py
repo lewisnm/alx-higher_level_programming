fdb1e8671d0be812e203aef1af2b56b8d9082084
Lewis Mutiso <lewisnmutiso@gmail.com>
2024-01-04 12:00:44 -0800

    solved the pycodestyle issue number 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import sys
 if __name__ == "__main__":
-    arg = [int(arg) for arg in sys.argv[1: ]]
+    arg = [int(arg) for arg in sys.argv[1:]]
     result = sum(arg)
     print(result)
