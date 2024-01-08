cf87417c5c79c9a0ffe26bcac9b7f075b0cdbc39
Lewis Mutiso <lewisnmutiso@gmail.com>
2024-01-08 08:03:00 -0800

    made the code more efficient

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    size = len(my_list)
-    if (idx < 0):
-        return(None)
-    elif ((idx > size) - 1):
+    if (idx < 0) or idx > len(my_list) - 1:
         return(None)
     else:
         return(my_list[idx])
