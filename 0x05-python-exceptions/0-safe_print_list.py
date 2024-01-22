141fdf62ea1ffc3fa8a292c50bf1926c17618dcd
Lewis Mutiso <lewisnmutiso@gmail.com>
2024-01-22 11:50:56 -0800

    made he file executable

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,10 @@
 #!/usr/bin/pytthon3
 def safe_print_list(my_list=[], x=0):
-    counter = 0
-    for i in range(x):
-        try:
-            print("{}".format(my_list[i]), end="")
-        except IndexError:
-            break
-        else:
-            counter += 1
+    try:
+        for value in my_list[:x]:
+            print(value, end='')
+    except IndexError:
+        print("\nIndex does not exist")
+        return 0
     print()
-    return counter
+    return len(my_list[:x])
