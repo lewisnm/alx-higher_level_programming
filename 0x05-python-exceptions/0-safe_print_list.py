c999c07eef89338c3357ac36fd7a76bfb3bd4501
Lewis Mutiso <lewisnmutiso@gmail.com>
2024-01-22 11:53:24 -0800

    changed the file again

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,12 @@
 #!/usr/bin/pytthon3
 def safe_print_list(my_list=[], x=0):
-    try:
-        for value in my_list[:x]:
-            print(value, end='')
-    except IndexError:
-        print("\nIndex does not exist")
-        return 0
+    counter = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+        except IndexError:
+            break
+        else:
+            counter += 1
     print()
-    return len(my_list[:x])
+    return counter
