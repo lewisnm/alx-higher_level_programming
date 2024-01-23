7f2c6f3e1db00363aac9fd425b068439f5969a6f
Lewis Mutiso <lewisnmutiso@gmail.com>
2024-01-23 12:53:11 -0800

    fixed the print messages

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -14,5 +14,5 @@ class Square:
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
-            raise ValueError("size must must be >= 0")
+            raise ValueError("size must be >= 0")
         self.__size = size
