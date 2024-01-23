a3a0ee57515a373e99d6a0c6089930dfc6f967bc
Lewis Mutiso <lewisnmutiso@gmail.com>
2024-01-23 12:25:56 -0800

    added the documentation to the class

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,5 +3,11 @@
 
 
 class Square:
+    """This is a method"""
     def __init__(self, size):
+        """This initializes the square
+
+        Args:
+            size(int): The square's size
+        """
         self.__size = size
